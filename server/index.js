f25a3193cdfb2f3256d8fbf654b9d03ed0479941
empodi <kglein9513@gmail.com>
2022-11-03 17:36:07 +0900

    postman login success

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 
 app.use(cors());
 
-const users = require('./routes/index');
+const users = require("./routes/index");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use('/users', users);
+app.use(morgan("dev"));
+app.use("/users", users);
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Server running on ${process.env.SERVER_PORT}`);
-})
+  console.log(`Server running on ${process.env.SERVER_PORT}`);
+});
