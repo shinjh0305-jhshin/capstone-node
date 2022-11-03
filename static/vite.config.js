a3648c3ca2816394633eee475fe6c608d21a087c
empodi <kglein9513@gmail.com>
2022-11-03 22:55:02 +0900

    FE - pug 적용 및 localhost:8080 서버 로그인 연결 1차 성공

--- static/vite.config.js
+++ static/vite.config.js
@@ -1,17 +1,22 @@
 // https://vitejs.dev/config/
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '/@': path.resolve(__dirname, './src'),
-      '/@components': path.resolve(__dirname, './src/components'),
-      '/@app_modules': path.resolve(__dirname, './src/modules'),
-      '/@stores': path.resolve(__dirname, './src/stores') 
-    }
-  }
-})
+      "/@": path.resolve(__dirname, "./src"),
+      "/@components": path.resolve(__dirname, "./src/components"),
+      "/@app_modules": path.resolve(__dirname, "./src/modules"),
+      "/@stores": path.resolve(__dirname, "./src/stores"),
+    },
+  },
+});
+
+module.rules = {
+  test: /\.pug$/,
+  loader: "pug-plain-loader",
+};
