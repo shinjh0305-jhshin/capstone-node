59b9f46455277b44c46fa230b581a9292db17cae
empodi <kglein9513@gmail.com>
2022-11-25 01:41:10 +0900

    ✅ 11.24 프론트엔드 + 채팅 서버 및 nodejs 더미 서버 완료

--- frontDB.sql
+++ frontDB.sql
@@ -42,44 +42,46 @@ CREATE TABLE Member (
     FOREIGN KEY(nickname) references User(nickname) on update cascade
 );
 
+CREATE TABLE product (
+    `id`            INT             NOT NULL    AUTO_INCREMENT,
+    `name`          VARCHAR(100)    NOT NULL,
+    `goal`          INT             NOT NULL,
+    `ordered`       INT             NOT NULL,
+    `people`        INT             NOT NULL    DEFAULT 1,
+    `price`         INT             NOT NULL    DEFAULT 0,
+    `url`           VARCHAR(200)    NOT NULL,
+    `portion`       INT             NOT NULL,
+    `unit`          VARCHAR(45)     NOT NULL,
+    `category`      INT             NOT NULL,
+    `tags`          VARCHAR(100)    NULL,
+    `ends`          DATE            NOT NULL,
+    `createdat`     DATETIME        NOT NULL    DEFAULT current_timestamp,
+    `createdby`     VARCHAR(100)    NOT NULL,
+    `deleted`       BOOLEAN         NOT NULL    DEFAULT 0 COMMENT '0-false 1-true',
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+    INDEX `IDX_SELLER` (`createdby` ASC, `createdat` ASC) INVISIBLE)
+COMMENT = '	';
+
+CREATE TABLE image (
+    `id`            INT             NOT NULL    AUTO_INCREMENT,
+    `product_id`    INT             NOT NULL,
+    `type`          INT             NOT NULL    DEFAULT 1   COMMENT '1-thumbnail\n2-제품 이미지',
+    `path`          VARCHAR(150)    NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `IDX_SEARCH` (`product_id` ASC, `type` ASC) VISIBLE
+);
+
+CREATE TABLE contents (
+    `id`            INT             NOT NULL    AUTO_INCREMENT,
+    `product_id`    INT             NOT NULL,
+    `content`       TEXT            NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `product_id_UNIQUE` (`product_id` ASC) VISIBLE
+);
+
 INSERT INTO User VALUES('admin', 'asdf1234', 'admin@gmail.com');
 INSERT INTO User VALUES('john', 'asdf1234', 'john@gmail.com');
 INSERT INTO User VALUES('jake', 'asdf1234', 'jake@gmail.com');
 INSERT INTO User VALUES('shin', 'asdf1234', 'shin@gmail.com');
 INSERT INTO User VALUES('park', 'asdf1234', 'park@gmail.com');
-
-CREATE TABLE product (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `goal` INT NOT NULL,
-  `ordered` INT NOT NULL,
-  `people` INT NOT NULL DEFAULT 1,
-  `price` INT NOT NULL DEFAULT 0,
-  `url` VARCHAR(200) NOT NULL,
-  `portion` INT NOT NULL,
-  `unit` VARCHAR(45) NOT NULL,
-  `category` INT NOT NULL,
-  `tags` VARCHAR(100) NULL,
-  `ends` DATE NOT NULL,
-  `createdat` DATETIME NOT NULL DEFAULT current_timestamp,
-  `createdby` VARCHAR(100) NOT NULL,
-  `deleted` BOOLEAN NOT NULL DEFAULT 0 COMMENT '0-false 1-true',
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `IDX_SELLER` (`createdby` ASC, `createdat` ASC) INVISIBLE)
-COMMENT = '	';
-
-CREATE TABLE image (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `product_id` INT NOT NULL,
-  `type` INT NOT NULL DEFAULT 1 COMMENT '1-thumbnail\n2-제품 이미지',
-  `path` VARCHAR(150) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `IDX_SEARCH` (`product_id` ASC, `type` ASC) VISIBLE);
-
-CREATE TABLE contents (
-`id` INT NOT NULL AUTO_INCREMENT,
-`product_id` INT NOT NULL,
-`content` TEXT NOT NULL,
-PRIMARY KEY (`id`),
-UNIQUE INDEX `product_id_UNIQUE` (`product_id` ASC) VISIBLE);
